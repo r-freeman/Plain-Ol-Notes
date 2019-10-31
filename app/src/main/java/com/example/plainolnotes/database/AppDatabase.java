c11b6c9c50ada031dfa4c578d62397240e422793
Ryan Freeman <N00173767@student.iadt.ie>
2019-10-31 15:28:36 +0000

    Added TypeConverter to manage date values

--- app/src/main/java/com/example/plainolnotes/database/AppDatabase.java
+++ app/src/main/java/com/example/plainolnotes/database/AppDatabase.java
@@ -5,15 +5,16 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 @Database(entities = {NoteEntity.class}, version = 1)
+@TypeConverters(DateConverter.class)
 public abstract class AppDatabase extends RoomDatabase {
     public static final String DATABASE_NAME = "AppDatabase.db";
     private static volatile AppDatabase instance;
     private static final Object LOCK = new Object();
 
     public abstract NoteDao noteDao();
-
 
     public static AppDatabase getInstance(Context context) {
         if(instance == null) {
