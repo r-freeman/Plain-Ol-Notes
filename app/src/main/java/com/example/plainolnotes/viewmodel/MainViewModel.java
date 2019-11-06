fbe9368753efb319cfd01bdbb8a9544c4c2110e8
Ryan Freeman <N00173767@student.iadt.ie>
2019-11-06 14:49:15 +0000

    Added repository class

--- app/src/main/java/com/example/plainolnotes/viewmodel/MainViewModel.java
+++ app/src/main/java/com/example/plainolnotes/viewmodel/MainViewModel.java
@@ -5,16 +5,19 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
+import com.example.plainolnotes.database.AppRepository;
 import com.example.plainolnotes.database.NoteEntity;
-import com.example.plainolnotes.utilities.SampleData;
 
 import java.util.List;
 
 public class MainViewModel extends AndroidViewModel {
 
-    public List<NoteEntity> mNotes = SampleData.getNotes();
+    public List<NoteEntity> mNotes;
+    private AppRepository mRepository;
 
     public MainViewModel(@NonNull Application application) {
         super(application);
+        mRepository = AppRepository.getInstance();
+        mNotes = mRepository.mNotes;
     }
 }
